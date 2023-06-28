d82a55fe9d6ee57595e17271f76eb4f0d096bda5
skyhawkgaming <trvs.wade@gmail.com>
2023-06-27 22:19:12 -0500

    fix application datatable

--- src/components/InfoTable.tsx
+++ src/components/InfoTable.tsx
@@ -1,6 +1,9 @@
-import { Text } from "@mantine/core";
+import { Stack, TextInput, Text } from "@mantine/core";
+import { useDebouncedValue } from "@mantine/hooks";
 import { StaffApplication } from "@prisma/client";
+import { IconSearch } from "@tabler/icons-react";
 import { DataTable } from "mantine-datatable";
+import { useEffect, useState } from "react";
 import { StaffApplicationForm } from "types/staffForm";
 
 export const InfoTable = ({
@@ -8,31 +11,62 @@ export const InfoTable = ({
 }: {
   applications: (StaffApplication | StaffApplicationForm)[];
 }) => {
+  const initialRecords = applications;
+  const [query, setQuery] = useState("");
+  const [records, setRecords] = useState<typeof applications>();
+  const [debouncedQuery] = useDebouncedValue(query, 200);
+
+  // search
+  useEffect(() => {
+    setRecords(
+      initialRecords.filter(({ osrsName }) => {
+        if (
+          debouncedQuery !== "" &&
+          !`${osrsName}`
+            .toLowerCase()
+            .includes(debouncedQuery.trim().toLowerCase())
+        ) {
+          return false;
+        }
+        return true;
+      })
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedQuery]);
+
   return (
     <div>
-      <DataTable
-        sx={{ height: "75vh" }}
-        withBorder
-        borderRadius="sm"
-        withColumnBorders
-        striped
-        highlightOnHover
-        columns={[
-          {
-            accessor: "osrsName",
-            title: "RSN",
-            render: (record) => <Text>{record.osrsName}</Text>,
-          },
-          // {
-          //   accessor: "id",
-          //   title: "id",
-          //   render: (value) => {
-          //     return value.id;
-          //   },
-          // },
-        ]}
-        records={applications}
-      />
+      <Stack>
+        <TextInput
+          placeholder="Search RSN..."
+          icon={<IconSearch size={16} />}
+          value={query}
+          onChange={(e) => setQuery(e.currentTarget.value)}
+        />
+        <DataTable
+          sx={{ height: "75vh" }}
+          withBorder
+          borderRadius="sm"
+          withColumnBorders
+          striped
+          highlightOnHover
+          columns={[
+            {
+              accessor: "osrsName",
+              title: "RSN",
+              render: ({ osrsName }) => <Text>{osrsName}</Text>,
+            },
+            // {
+            //   accessor: "id",
+            //   title: "id",
+            //   render: (value) => {
+            //     return value.id;
+            //   },
+            // },
+          ]}
+          records={records}
+        />
+      </Stack>
     </div>
   );
 };
